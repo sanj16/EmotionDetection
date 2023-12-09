5ec96335825225c090a07efe83b3e86dffcab99f
sanj16 <sanjana.mandarapu@gmail.com>
2023-12-09 12:50:04 +0530

    done

--- TrainEmotion.py
+++ TrainEmotion.py
@@ -1,16 +1,12 @@
-
-# import required packages
 import cv2
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
-# Initialize image data generator with rescaling
 train_data_gen = ImageDataGenerator(rescale=1./255)
 validation_data_gen = ImageDataGenerator(rescale=1./255)
 
-# Preprocess all test images
 train_generator = train_data_gen.flow_from_directory(
         'data/train',
         target_size=(48, 48),
@@ -18,7 +14,6 @@ train_generator = train_data_gen.flow_from_directory(
         color_mode="grayscale",
         class_mode='categorical')
 
-# Preprocess all train images
 validation_generator = validation_data_gen.flow_from_directory(
         'data/test',
         target_size=(48, 48),
@@ -26,7 +21,6 @@ validation_generator = validation_data_gen.flow_from_directory(
         color_mode="grayscale",
         class_mode='categorical')
 
-# create model structure
 emotion_model = Sequential()
 
 emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
@@ -49,7 +43,6 @@ cv2.ocl.setUseOpenCL(False)
 
 emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
 
-# Train the neural network/model
 emotion_model_info = emotion_model.fit_generator(
         train_generator,
         steps_per_epoch=28709 // 64,
@@ -57,11 +50,9 @@ emotion_model_info = emotion_model.fit_generator(
         validation_data=validation_generator,
         validation_steps=7178 // 64)
 
-# save model structure in jason file
 model_json = emotion_model.to_json()
 with open("emotion_model.json", "w") as json_file:
     json_file.write(model_json)
 
-# save trained model weight in .h5 file
 emotion_model.save_weights('emotion_model.h5')
 
